91c61ac381f3d37a0a536926bf6bb602f2076093
Mark Tomcza <marktomcza@Mark-Tomcza-Mac.lan>
2021-11-01 23:56:58 -0500

    test

--- docker-action/entrypoint-az.sh
+++ docker-action/entrypoint-az.sh
@@ -188,17 +188,8 @@ echo "successfully pushed container image to registry."
 echo "-------------------------------------------"
 
 # install kubectl
-echo "++installing kubectl via yum package manager..."
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-yum install -y kubectl
+echo "++installing kubectl..."
+apk add kubectl
 echo "++successfully installed kubectl"
 echo "-------------------------------------------"
 
